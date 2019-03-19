5f85cfaba7c2c5b969dca31dc56c6721391b0ae9
yubing1987 <ybjx1987@qq.com>
2019-03-20 00:00:42 +0800

    添加了第26题的解法

---
+++ solution/src/main/java/com/ybjx/leetcode/solution/RemoveDuplicates26.java
@@ -0,0 +1,28 @@
+package com.ybjx.leetcode.solution;
+
+import com.ybjx.leetcode.annotation.LeetCodeSolution;
+import com.ybjx.leetcode.common.AbstractSolution;
+
+/**
+ * @author ybjx
+ * @date 2019/3/19 23:51
+ */
+public class RemoveDuplicates26 extends AbstractSolution {
+
+    @LeetCodeSolution
+    public int solution(int [] nums){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int len = 0;
+        int p = 1;
+        while(p < nums.length){
+            if(nums[p] != nums[len]){
+                len ++;
+                nums[len] = nums[p];
+            }
+            p ++;
+        }
+        return len + 1;
+    }
+}
