39c38b5818b045a85d60f8d482258ad4a17d83c2
yubing1987 <ybjx1987@qq.com>
2019-04-14 09:41:36 +0800

    完成了007题的文章链接

--- solution/src/main/java/com/ybjx/leetcode/solution/L007Reverse.java
+++ solution/src/main/java/com/ybjx/leetcode/solution/L007Reverse.java
@@ -23,7 +23,6 @@ public class L007Reverse extends AbstractSolution {
     public int solution(int x){
         int r = 0;
         while(x != 0){
-
             int pop = x % 10;
             x /= 10;
             if (r > Integer.MAX_VALUE/10 || (r == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
