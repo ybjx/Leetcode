05d9b13ca21d3176e421b44e31618083f563da3a
ybjx <ybjx@qq.com>
2020-08-01 00:00:38 +0800

    添加95题解法

--- framework/src/main/java/com/ybjx/leetcode/type/TreeNode.java
+++ framework/src/main/java/com/ybjx/leetcode/type/TreeNode.java
@@ -1,5 +1,7 @@
 package com.ybjx.leetcode.type;
 
+import com.ybjx.leetcode.common.Constant;
+
 public class TreeNode {
     /**
      * 值
@@ -19,4 +21,65 @@ public class TreeNode {
      * @param x 值
      */
     public TreeNode(int x) { val = x; }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){
+            return false;
+        }
+        if(!(obj instanceof TreeNode)){
+            return false;
+        }
+        TreeNode node = (TreeNode)obj;
+        if(this.val != node.val){
+            return false;
+        }
+        if(this.left != null){
+            if(!this.left.equals(node.left)){
+                return false;
+            }
+        }
+        else{
+            if(node.left != null){
+                return false;
+            }
+        }
+        if(this.right != null){
+            return this.right.equals(node.right);
+        }
+        else{
+            return node.right == null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(Constant.LEFT_SQUARE_BRACKET).append(this.val).append(Constant.COMMA);
+        String n;
+        if(left != null){
+            n = left.toString();
+            n = n.substring(1, n.length() - 1);
+        }
+        else{
+            n = "null";
+        }
+        builder.append(n).append(Constant.COMMA);
+        if(right != null){
+            n = right.toString();
+            n = n.substring(1, n.length() - 1);
+        }
+        else{
+            n = "null";
+        }
+        builder.append(n);
+        builder.append(Constant.RIGHT_SQUARE_BRACKET);
+        return builder.toString();
+    }
 }
---
+++ solution/src/main/java/com/ybjx/leetcode/solution/_0001_0100/L095GenerateTrees.java
@@ -0,0 +1,69 @@
+package com.ybjx.leetcode.solution._0001_0100;
+
+import com.ybjx.leetcode.annotation.LeetCodeSolution;
+import com.ybjx.leetcode.common.AbstractSolution;
+import com.ybjx.leetcode.type.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 题目地址：https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
+ * 题目：
+ *     给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ *
+ * 示例：
+ *     输入：3
+ *     输出：
+ *       [
+ *         [1,null,3,2],
+ *         [3,2,null,1],
+ *         [3,1,null,null,2],
+ *         [2,1,3],
+ *         [1,null,2,null,3]
+ *       ]
+ *     解释：
+ *       以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *         1         3     3      2      1
+ *          \       /     /      / \      \
+ *           3     2     1      1   3      2
+ *          /     /       \                 \
+ *         2     1         2                 3
+ */
+public class L095GenerateTrees extends AbstractSolution {
+
+    @LeetCodeSolution
+    public List<TreeNode> solution(int n){
+        return calc(1, n);
+    }
+
+    private List<TreeNode> calc(int s, int e){
+        List<TreeNode> list = new ArrayList<>();
+        if(s > e){
+            return list;
+        } else if(s == e){
+            TreeNode node = new TreeNode(s);
+            list.add(node);
+            return list;
+        }
+        for(int i = s; i <= e; i++){
+            List<TreeNode> l1 = calc(s, i - 1);
+            List<TreeNode> l2 = calc(i + 1, e);
+            if(l1.size() == 0){
+                l1.add(null);
+            }
+            if(l2.size() == 0){
+                l2.add(null);
+            }
+            for (TreeNode t1 : l1) {
+                for (TreeNode t2 : l2) {
+                    TreeNode r = new TreeNode(i);
+                    r.left = t1;
+                    r.right = t2;
+                    list.add(r);
+                }
+            }
+        }
+        return list;
+    }
+}
