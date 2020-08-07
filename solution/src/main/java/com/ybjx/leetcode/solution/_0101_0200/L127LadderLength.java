45e71dda60e985be05ccd1a52be774ce76db22bf
ybjx <ybjx@qq.com>
2020-08-07 23:00:49 +0800

    添加127题解法

---
+++ solution/src/main/java/com/ybjx/leetcode/solution/_0101_0200/L127LadderLength.java
@@ -0,0 +1,124 @@
+package com.ybjx.leetcode.solution._0101_0200;
+
+import com.ybjx.leetcode.annotation.LeetCodeSolution;
+import com.ybjx.leetcode.common.AbstractSolution;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 题目地址：https://leetcode-cn.com/problems/word-ladder/
+ * 题目：
+ *     给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+ *     每次转换只能改变一个字母。
+ *     转换过程中的中间单词必须是字典中的单词。
+ *
+ * 说明:
+ *     如果不存在这样的转换序列，返回 0。
+ *     所有单词具有相同的长度。
+ *     所有单词只由小写字母组成。
+ *     字典中不存在重复的单词。
+ *     你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+ *
+ * 示例 1:
+ *     输入:
+ *       beginWord = "hit",
+ *       endWord = "cog",
+ *       wordList = ["hot","dot","dog","lot","log","cog"]
+ *     输出: 5
+ *     解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ *     返回它的长度 5。
+ *
+ * 示例 2:
+ *     输入:
+ *       beginWord = "hit"
+ *       endWord = "cog"
+ *       wordList = ["hot","dot","dog","lot","log"]
+ *     输出: 0
+ *     解释: endWord "cog" 不在字典中，所以无法进行转换。
+ */
+public class L127LadderLength extends AbstractSolution {
+
+
+    private Map<String, List<String>> map = new HashMap<>();
+
+    private Map<String, Integer> cache = new HashMap<>();
+
+    @LeetCodeSolution
+    public int solution(String beginWord, String endWord, List<String> wordList){
+        if(beginWord.length() == 0){
+            return 0;
+        }
+        wordList.add(0, beginWord);
+        if(!wordList.contains(endWord)){
+            return 0;
+        }
+        for(int i = 0; i < wordList.size(); i++){
+            String s = wordList.get(i);
+            cache.put(s, Integer.MAX_VALUE);
+            if(s.equals(endWord)){
+                continue;
+            }
+            List<String> l = new ArrayList<>();
+            map.put(s, l);
+            for(String s1: wordList){
+                if(isOk(s, s1)){
+                    l.add(s1);
+                }
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        List<String> l = new ArrayList<>();
+        l.add(beginWord);
+        result.add(l);
+        return calc(result, endWord);
+    }
+
+    private int calc(List<List<String>> list,  String endWord){
+        List<List<String>> r = new ArrayList<>();
+        for(List<String> l: list){
+            if(l.contains(endWord)){
+                r.add(l);
+            }
+        }
+        if(r.size() > 0){
+            return r.get(0).size();
+        }
+        for(List<String> l: list){
+            List<String> t = map.get(l.get(l.size() - 1));
+            if(t.size() == 0){
+                continue;
+            }
+            for(String s: t){
+                if(l.contains(s)){
+                    continue;
+                }
+                if(cache.get(s) < l.size() + 1){
+                    continue;
+                }
+                cache.put(s, l.size() + 1);
+                List<String> ll = new ArrayList<>(l);
+                ll.add(s);
+                r.add(ll);
+            }
+        }
+        if(r.size() == 0){
+            return 0;
+        }
+        else{
+            return calc(r, endWord);
+        }
+    }
+
+    private boolean isOk(String s1, String s2){
+        int c = 0;
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                c++;
+            }
+        }
+        return c == 1;
+    }
+}
